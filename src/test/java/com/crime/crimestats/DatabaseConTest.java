6fc51173232648cd678069f0bfd0830730a01661
jgarciasol <99057885+jgarciasol@users.noreply.github.com>
2024-03-17 21:44:38 -0400

    tested db connection

---
+++ src/test/java/com/crime/crimestats/DatabaseConTest.java
@@ -0,0 +1,10 @@
+package com.crime.crimestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DatabaseConTest {
+}

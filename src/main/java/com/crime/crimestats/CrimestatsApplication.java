4c9e8b15ef0f299dab48fddb9f33dff80582ae11
jgarciasol <99057885+jgarciasol@users.noreply.github.com>
2024-03-17 21:20:34 -0400

    dependencies for mvn project

---
+++ src/main/java/com/crime/crimestats/CrimestatsApplication.java
@@ -0,0 +1,13 @@
+package com.crime.crimestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimestatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimestatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crime/crimestats/CrimestatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crime.crimestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimestatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

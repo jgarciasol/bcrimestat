d7917d556fffd3bddf893ef006a0ae36939e6cdd
jgarciasol <99057885+jgarciasol@users.noreply.github.com>
2024-03-23 15:06:41 -0400

    edited crimecode class

--- src/main/java/com/crime/crimestats/entity/CrimeCode.java
+++ src/main/java/com/crime/crimestats/entity/CrimeCode.java
@@ -1,17 +1,7 @@
 package com.crime.crimestats.entity;
 
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
 public class CrimeCode {
 
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long crimeCodeId;
     private String crimeCodeName;
     private String description;

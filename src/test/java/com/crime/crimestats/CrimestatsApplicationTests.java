a1e030da3cd23584eba94743a6b0ce5f17b87fb0
jgarciasol <99057885+jgarciasol@users.noreply.github.com>
2024-03-23 12:59:38 -0400

    combined db connection test

--- src/test/java/com/crime/crimestats/CrimestatsApplicationTests.java
+++ src/test/java/com/crime/crimestats/CrimestatsApplicationTests.java
@@ -1,13 +1,24 @@
 package com.crime.crimestats;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CrimestatsApplicationTests {
-
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testDBConnection() {
+		Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+		assertThat(result).isNotNull();
 	}
 
 }

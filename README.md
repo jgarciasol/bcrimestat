df005403e8108c863840a5ecfaa9868374cf3835
jgarciasol <99057885+jgarciasol@users.noreply.github.com>
2024-11-17 19:08:53 -0500

    ran swagger page and can view all crimecodes

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ This is a learning project.
 1. I need to get the backend working first; creating some endpoints
 2. learn some React to get the front-end working.
 3. .... Not sure yet.
+
+Update:
+I set up swagger, and can get to the swagger pages by running the app
+Then going here: http://localhost:8080/swagger-ui.html
+Raw OpenAPI documentation: http://localhost:8080/v3/api-docs
+
+
--- src/main/java/com/crime/crimestats/controller/CrimeCodeController.java
+++ src/main/java/com/crime/crimestats/controller/CrimeCodeController.java
@@ -1,16 +1,20 @@
 package com.crime.crimestats.controller;
 
 import com.crime.crimestats.entity.CrimeCode;
+import com.crime.crimestats.service.CrimeCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+
+@RestController
+@RequestMapping("/api/crimecodes")
 public class CrimeCodeController {
-    private final CrimeCodeController crimeCodeService;
+    private final CrimeCodeService crimeCodeService;   // Correct
 
     @Autowired
-    public CrimeCodeController(CrimeCodeController crimeCodeService) {
+    public CrimeCodeController(CrimeCodeService crimeCodeService) {
         this.crimeCodeService = crimeCodeService;
     }
 
